fe3376464aab21cf053a0fe241b130712fe84d6b
Gaurav <gaurav.k@taboola.com>
2020-11-02 09:52:00 +0530

    Removed unused library and update TaboolaSampleApplication.java

--- app/src/main/java/com/taboola/sdk3example/TaboolaSampleApplication.java
+++ app/src/main/java/com/taboola/sdk3example/TaboolaSampleApplication.java
@@ -1,17 +1,7 @@
 package com.taboola.sdk3example;
 import android.app.Application;
-import android.content.Context;
-import android.graphics.drawable.Drawable;
-
-import androidx.core.content.res.ResourcesCompat;
-
 import com.taboola.android.TBLPublisherInfo;
 import com.taboola.android.Taboola;
-
-import java.util.HashMap;
-import java.util.Map;
-
-
 public class TaboolaSampleApplication extends  Application{
 
 
@@ -19,12 +9,7 @@ public class TaboolaSampleApplication extends  Application{
     @Override
     public void onCreate() {
         super.onCreate();
-
-         tblPublisherInfo  = new TBLPublisherInfo("sdk-tester-demo").setApiKey("30dfcf6b094361ccc367bbbef5973bdaa24dbcd6");
-        Map<String, String> taboolaExtraProperties = new HashMap<>();
-        taboolaExtraProperties.put("enableFullRawDataResponse", "true");
-        taboolaExtraProperties.put("allowNonOrganicClickOverride", "true");
-        Drawable imagePlaceholder = ResourcesCompat.getDrawable(getApplicationContext().getResources(), R.drawable.image_placeholder, null);
+        tblPublisherInfo  = new TBLPublisherInfo("sdk-tester-demo").setApiKey("30dfcf6b094361ccc367bbbef5973bdaa24dbcd6");
         Taboola.init(tblPublisherInfo);
     }
 
