ed13edd1c708438bf266f90371cd729a9e433ad0
Rotem Tal <rotem.t@taboola.com>
2021-04-29 12:25:53 +0300

    Scrollview Widget + Feed:
    - Refactored Scrollview example

--- Java/app/src/main/java/com/taboola/sdk3example/sdk_classic/FeedWithMiddleArticleInsideScrollViewFragment.java
+++ Java/app/src/main/java/com/taboola/sdk3example/sdk_classic/FeedWithMiddleArticleInsideScrollViewFragment.java
@@ -21,72 +21,68 @@ import java.util.HashMap;
 
 
 public class FeedWithMiddleArticleInsideScrollViewFragment extends Fragment  {
-    private static final String TAG = "DEBUG";
+    private static final String TAG = "FeedWithMiddleArticleInsideScrollViewFragment";
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_standard, container, false);
 
+        // Create a Taboola page
         TBLClassicPage tblClassicPage=Taboola.getClassicPage(getContext(), "https://blog.taboola.com", "article");
-        buildMiddleArticleWidget(view.findViewById(R.id.taboola_widget_middle), tblClassicPage);
-        buildBelowArticleWidget(view.findViewById(R.id.taboola_widget_below_article), tblClassicPage);
+
+        // Create Taboola units for the page
+        TBLClassicUnit tblClassicUnitMiddleArticle = view.findViewById(R.id.taboola_widget_middle);
+        TBLClassicUnit tblClassicUnitBelowArticle = view.findViewById(R.id.taboola_widget_below_article);
+
+        // Configure and add the units the page
+        configureMiddleArticleWidget(tblClassicUnitMiddleArticle, tblClassicPage);
+        configureBelowArticleWidget(tblClassicUnitBelowArticle, tblClassicPage);
+
+        // Fetch content for each unit
+        tblClassicUnitMiddleArticle.fetchContent();
+        tblClassicUnitBelowArticle.fetchContent();
         return view;
     }
-    private void buildMiddleArticleWidget(TBLClassicUnit tblClassicUnit, TBLClassicPage tblClassicPage) {
-        tblClassicUnit
-                .setPublisherName("sdk-tester-demo")
-                .setPageType("article")
-                .setPageUrl("https://blog.taboola.com")
-                .setPlacement("Mid Article")
-                .setMode("alternating-widget-without-video-1x1")
-                .setTargetType("mix"); // setViewId - used in order to prevent duplicate recommendations between widgets on the same page view
+    private void configureMiddleArticleWidget(TBLClassicUnit tblClassicUnit, TBLClassicPage tblClassicPage) {
+        tblClassicUnit.setTargetType("mix");
 
-
-        TBLClassicListener tblClassicListener;
-        tblClassicListener=new TBLClassicListener() {
+        TBLClassicListener tblClassicListener =new TBLClassicListener() {
             @Override
             public boolean onItemClick(String placementName, String itemId, String clickUrl, boolean isOrganic, String customData) {
                 return super.onItemClick(placementName, itemId, clickUrl, isOrganic, customData);
             }
         };
-        tblClassicPage.addUnitToPage(tblClassicUnit,"Mid Article","alternating-widget-without-video-1x1", TBL_PLACEMENT_TYPE.FEED,tblClassicListener);
-        tblClassicUnit.fetchContent();
+        tblClassicPage.addUnitToPage(tblClassicUnit,
+                "Mid Article",
+                "alternating-widget-without-video-1x1",
+                TBL_PLACEMENT_TYPE.FEED,
+                tblClassicListener);
+       ;
 
     }
 
-    private void buildBelowArticleWidget(TBLClassicUnit tblClassicUnit, TBLClassicPage tblClassicPage) {
-        tblClassicUnit
-                .setPublisherName("sdk-tester-demo")
-                .setPageType("article")
-                .setPageUrl("https://blog.taboola.com")
-                .setPlacement("Feed without video")
-                .setMode("thumbs-feed-01")
-                .setTargetType("mix")
-                .setInterceptScroll(true);
+    private void configureBelowArticleWidget(TBLClassicUnit tblClassicUnit, TBLClassicPage tblClassicPage) {
+        tblClassicUnit.setTargetType("mix");
+        tblClassicUnit.setInterceptScroll(true);
 
         tblClassicUnit.getLayoutParams().height = TBLSdkDetailsHelper.getDisplayHeight( tblClassicUnit.getContext()) * 2;
         HashMap<String, String> extraProperties = new HashMap<>();
         extraProperties.put("useOnlineTemplate", "true");
         extraProperties.put("detailedErrorCodes", "true");
-        TBLClassicListener tblClassicListener;
-        tblClassicListener=new TBLClassicListener() {
+        TBLClassicListener tblClassicListener = new TBLClassicListener() {
             @Override
             public boolean onItemClick(String placementName, String itemId, String clickUrl, boolean isOrganic, String customData) {
-                Log.d(TAG,"onItemClick");
-                return super.onItemClick(placementName, itemId, clickUrl, isOrganic, customData);
+                  return super.onItemClick(placementName, itemId, clickUrl, isOrganic, customData);
             }
         };
 
         tblClassicUnit.setUnitExtraProperties(extraProperties);
         tblClassicPage.addUnitToPage( tblClassicUnit,"Feed without video","thumbs-feed-01", TBL_PLACEMENT_TYPE.FEED,tblClassicListener);
-        tblClassicUnit.setUnitExtraProperties(extraProperties);
-        tblClassicUnit.fetchContent();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d(TAG,"onDestroy");
     }
 }
