27c0da28e519daa9d60f2b2962f10d5d8d8d1610
Slava Voltfman <slava.v@taboola.com>
2023-03-29 15:17:02 +0300

    MOB-1658 - Simplify the compose SDK sample code to improve clarity - pr fixes

--- Kotlin/app/src/main/java/com/taboola/kotlin/examples/screens/web/TaboolaWebWrapperViewModel.kt
+++ Kotlin/app/src/main/java/com/taboola/kotlin/examples/screens/web/TaboolaWebWrapperViewModel.kt
@@ -9,19 +9,25 @@ import com.taboola.android.listeners.TBLWebListener
 import com.taboola.android.tblweb.TBLWebUnit
 import com.taboola.kotlin.examples.PlacementInfo
 
-class TaboolaWebWrapperViewModel: ViewModel() {
+class TaboolaWebWrapperViewModel : ViewModel() {
 
     /**
      * Build a Taboola Page and Unit to describe the screen and placement you wish Taboola to work with.
      * Notice: Taboola requires JavaScript to be enabled in the WebView to work.
      */
     @SuppressLint("SetJavaScriptEnabled")
-    fun setupWebViewForTaboola(webView: WebView) : TBLWebUnit {
+    fun setupWebViewForTaboola(webView: WebView): TBLWebUnit {
         //If not already set in your WebView
         webView.settings.javaScriptEnabled = true
 
-        return Taboola.getWebPage().build(webView, object: TBLWebListener(){
-            override fun onItemClick(placementName: String?, itemId: String?, clickUrl: String?, isOrganic: Boolean, customData: String?): Boolean {
+        return Taboola.getWebPage().build(webView, object : TBLWebListener() {
+            override fun onItemClick(
+                placementName: String?,
+                itemId: String?,
+                clickUrl: String?,
+                isOrganic: Boolean,
+                customData: String?
+            ): Boolean {
                 println("Taboola | onItemClick | isOrganic = $isOrganic")
                 return super.onItemClick(placementName, itemId, clickUrl, isOrganic, customData)
             }
@@ -32,14 +38,18 @@ class TaboolaWebWrapperViewModel: ViewModel() {
      * This method is just a quick way to load an example page simulating customer layout.
      * Inside the asset file there are Javascript tags Taboola targets with its content.
      */
-    fun loadWebViewContentWidget(webView: WebView, properties: PlacementInfo.WidgetProperties, context: Context) {
+    fun loadWebViewContentWidget(
+        webView: WebView,
+        properties: PlacementInfo.WidgetProperties,
+        context: Context
+    ) {
         val BASE_URL = "https://example.com"
         val HTML_CONTENT_FILE_TITLE = "sampleContentPage.html"
-        var htmlContent: String? = null
         try {
-            htmlContent = AssetUtil.getHtmlTemplateFileContent(context, HTML_CONTENT_FILE_TITLE)
-            htmlContent = htmlContent.replace("<PLACEMENT>", properties.placementName)
-            htmlContent = htmlContent.replace("<MODE>", properties.mode)
+            val htmlContent: String =
+                AssetUtil.getHtmlTemplateFileContent(context, HTML_CONTENT_FILE_TITLE)
+                    .replace("<PLACEMENT>", properties.placementName)
+                    .replace("<MODE>", properties.mode)
             webView.loadDataWithBaseURL(BASE_URL, htmlContent, "text/html", "UTF-8", "")
         } catch (e: Exception) {
             println("Failed to read asset file: ${e.localizedMessage}")
@@ -51,14 +61,18 @@ class TaboolaWebWrapperViewModel: ViewModel() {
      * This method is just a quick way to load an example page simulating customer layout.
      * Inside the asset file there are Javascript tags Taboola targets with its content.
      */
-    fun loadWebViewContentFeed(webView: WebView, properties: PlacementInfo.WebFeedProperties, context: Context) {
+    fun loadWebViewContentFeed(
+        webView: WebView,
+        properties: PlacementInfo.WebFeedProperties,
+        context: Context
+    ) {
         val BASE_URL = "https://example.com"
         val HTML_CONTENT_FILE_TITLE = "sampleContentPage.html"
-        var htmlContent: String? = null
         try {
-            htmlContent = AssetUtil.getHtmlTemplateFileContent(context, HTML_CONTENT_FILE_TITLE)
-            htmlContent = htmlContent.replace("<PLACEMENT>", properties.placementName)
-            htmlContent = htmlContent.replace("<MODE>", properties.mode)
+            val htmlContent: String =
+                AssetUtil.getHtmlTemplateFileContent(context, HTML_CONTENT_FILE_TITLE)
+                    .replace("<PLACEMENT>", properties.placementName)
+                    .replace("<MODE>", properties.mode)
             webView.loadDataWithBaseURL(BASE_URL, htmlContent, "text/html", "UTF-8", "")
         } catch (e: Exception) {
             println("Failed to read asset file: ${e.localizedMessage}")
